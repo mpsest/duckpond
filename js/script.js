45df4ccfb962123eb701e72431b20636c3db432f
mpsest <mpsest6@gmail.com>
2025-07-20 16:24:40 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// window.addEventListener("DOMContentLoaded", function () {
+//   const container = document.querySelector(".sections-container");
+//   if (container) {
+//     // Scroll to the center cell (center section)
+//     container.scrollTo({
+//       left: container.scrollWidth / 3,
+//       top: container.scrollHeight / 3,
+//       behavior: "auto",
+//     });
+//   }
+// });
+
+const button = document.getElementById("buttonLP");
+button.addEventListener("click", function () {
+  alert("OLA");
+});
